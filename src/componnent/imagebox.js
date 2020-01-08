3e0164a8d0bb5eaafbd25f43657806d0f2e3c48b
Hossein <h_marjany@yahoo.com>
2020-01-08 21:17:43 +0330

    fix web

--- src/componnent/imagebox.js
+++ src/componnent/imagebox.js
@@ -4,7 +4,7 @@ import config from "./../auth_config.json";
 export default function ImageBox(props) {
 
     return (
-        <div ratio="3/4" className='ImageBox' style={{ backgroundImage: `url(${config.server}/${props.ImagePath})`, backgroundSize: 'cover' }}>
+        <div ratio="3/4" className='ImageBox' style={{ backgroundImage: `url(http://marjani.eu-4.evennode.com/${props.ImagePath})`, backgroundSize: 'cover' }}>
 
         </div>
     );
