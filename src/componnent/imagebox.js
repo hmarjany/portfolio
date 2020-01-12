243a2f8cb41d62f9f425390e4eaad0c220013f55
Hossein <h_marjany@yahoo.com>
2020-01-12 23:04:41 +0330

    fix web

--- src/componnent/imagebox.js
+++ src/componnent/imagebox.js
@@ -1,14 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import config from "./../auth_config.json";
+import axios from 'axios';
 
 export default function ImageBox(props) {
 
-    var url = $http.get(`${config.server}/${props.ImagePath}`, {
-        headers: {'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'}
-    });
+    const[url,setUrl]=useState('');
+    useEffect(() => {
+        getImage();
+    },[url])
 
+    var getImage = async ()=>{
+        axios.get(`${config.server}/${props.ImagePath}`,
+            { responseType: 'arraybuffer',headers: { 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'}})
+            .then((res) => {
+                var url = '"data:image/jpg;base64,'+Buffer.from(res.data, 'binary').toString('base64')+'"';
+                setUrl(url);
+            });
+    }
     return (
-        <div ratio="3/4" className='ImageBox' style={{ backgroundImage: `url(${url})`, backgroundSize: 'cover' }}>
+        
+        <div ratio="3/4" className='ImageBox' style={{ backgroundImage:'url('+url+')', backgroundSize: 'cover' }}>
 
         </div>
     );
